46fca5dabf67f30887077b1472d2cba4b2abe2b5
deLears <91895629+deLears@users.noreply.github.com>
2021-10-07 16:07:13 +0300

    Add files via upload

---
+++ Podzadanie1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Podzadanie1
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры предложение.
+            Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.
+            Найти самое длинное слово в строке.
+            */
+            Console.Write("Введите строку: ");
+            string vvod = Console.ReadLine();
+            string[] str = vvod.Split(new Char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            int max = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > max)
+                {
+                    max = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", str[index]);
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ Podzadanie3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Podzadanie3
+    {
+        static void Main(string[] args)
+        {
+            /*Дана строка S.Из строки требуется удалить текст, заключенный в фигурные скобки.
+             * В строке может быть несколько фрагментов, заключённых в фигурные скобки.Возможно использование вложенных фигурных скобок,
+             * необходимо, чтобы программа это учитывала.
+             */
+            Console.Write("Введите строку с некоторым количеством фигурных скобок типа");
+            Console.WriteLine(@"{ или }");
+            string s = Console.ReadLine();
+            int sL = s.Length;
+            
+            char[] arrC = new char[sL]; 
+            char[] arrS = new char[sL];
+            arrS = s.ToCharArray();
+            
+            for (int i = 0; i < sL; i++)
+            {
+                if ((arrS[i] == '{') || (arrS[i] == '}'))
+                {
+                    arrC[i] = arrS[i];
+                }              
+            }
+            StringBuilder sb = new StringBuilder(arrC.Length);
+            string value = sb.Append(arrC).ToString(); 
+            Console.WriteLine("Новая строка: {0}", sb);
+            Console.ReadKey();
+        }
+               
+    }
+}
+
+
