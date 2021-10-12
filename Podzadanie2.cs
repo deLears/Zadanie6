b6f47acdddbbbc20a9dfcd259e8d51829ce6ec1b
deLears <91895629+deLears@users.noreply.github.com>
2021-10-12 17:59:36 +0300

    Add files via upload

---
+++ Podzadanie2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Podzadanie2
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.
+                         * Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра
+                         * (пример палиндрома – «А роза упала на лапу Азора»).
+                         */
+            Console.WriteLine("Введите предложение для проверки палиндрома");
+            string pred = Console.ReadLine();
+            string tpred = pred.ToLower();
+            bool IsPalindrome = true;
+            
+            int i = 0, j = tpred.Length - 1;
+                while (i < j)
+                {
+                    if (char.IsWhiteSpace(tpred[i]) || char.IsPunctuation(tpred[i]))
+                        i++;
+                    else if (char.IsWhiteSpace(tpred[j]) || char.IsPunctuation(tpred[j]))
+                        j--;
+                    else if (char.ToLowerInvariant(tpred[i++]) != char.ToLowerInvariant(tpred[j--]))
+                    IsPalindrome =false;
+                }
+
+
+            if (IsPalindrome == true)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else Console.WriteLine("НЕТ");
+            Console.ReadKey();
+
+        }
+    }
+}
